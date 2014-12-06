cd274bd1f8965fac6fe110a5bdea3e6985d26fb2
tharindu99 <madushanka.best@gmail.com>
2014-12-06 20:54:59 +0530

    add project basic operations in first level

---
+++ TinyURL/src/TinyURL.c
@@ -0,0 +1,74 @@
+/*
+ ============================================================================
+ Name        : TinyURL.c
+ Author      : Tharindu  Madushanka
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+#include <string.h>
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+	int i;
+	for (i = 0; i < argc; i++) {
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+	printf("\n");
+	return 0;
+}
+
+int main(int argc, char* argv[]) {
+
+	char *URL = "WWW.GOOGLE.COM";
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
+	char *sql;
+	const char *string = "The quick brown fox jumped over the lazy dog's back";
+
+	// open sqlite3 database "URL_Records"
+	rc = sqlite3_open("URL_Records.db", &db);
+
+	// if db exist it will open otherwise create a db
+	if (rc) {
+		fprintf(stderr, "can't open");
+		exit(0);
+	} else {
+		fprintf(stdout, "opened");
+	}
+
+	//query to create "Records" table if not exists.
+	sql = "CREATE TABLE if not exists Records("
+			"Id INTEGER PRIMARY KEY  AUTOINCREMENT,"
+			"URL           TEXT    NOT NULL,"
+			"Hash          CHAR(5)     NOT NULL);";
+	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+	//Execute the sql statement.
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
+	} else {
+		fprintf(stdout, "Table created successfully\n");
+	}
+
+
+	sql = "SELECT * from Records";
+
+	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+	//Execute the sql statement.
+		if (rc != SQLITE_OK) {
+			fprintf(stderr, "SQL error: %s\n", zErrMsg);
+			sqlite3_free(zErrMsg);
+		} else {
+			fprintf(stdout, "operation successfully\n");
+		}
+		sqlite3_close(db);
+
+
+}
