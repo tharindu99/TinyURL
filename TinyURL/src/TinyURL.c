296be2474390c545b3cfeddad7f00a87d2093f83
tharindu99 <madushanka.best@gmail.com>
2014-12-06 23:31:35 +0530

    change the db structure and callback function into single value

--- TinyURL/src/TinyURL.c
+++ TinyURL/src/TinyURL.c
@@ -14,24 +14,28 @@
 #include <string.h>
 #include <openssl/sha.h>
 
-static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+static int callback(void *data, int argc, char **argv, char **azColName) {
+	// this function for print the database retrivals.
 	int i;
+	printf("%s\n :",argv[0]);
+	/*
+	 * if you want to print all the retrivals from query
 	for (i = 0; i < argc; i++) {
 		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-	}
-	printf("\n");
+	}*/
 	return 0;
 }
 
 static char* hash_generator(char *word) {
+	//this function for create the hash value for string
 	int i = 0;
 	unsigned char temp[SHA_DIGEST_LENGTH];
 	char buf[SHA_DIGEST_LENGTH * 2];
-	//char *word = "tharigdddfndu";
 
 	memset(buf, 0x0, SHA_DIGEST_LENGTH * 2);
 	memset(temp, 0x0, SHA_DIGEST_LENGTH);
 
+	//hash generate using SHA1 algorithm
 	SHA1((unsigned char *) word, strlen(word), temp);
 
 	for (i = 0; i < 4; i++) {
@@ -40,15 +44,20 @@ static char* hash_generator(char *word) {
 	return buf;
 
 }
+/*
+static void show(char *hash_value){
+
+}*/
 
 int main(int argc, char* argv[]) {
 
-	char *URL = "WWW.GOOGLE.COM";
+	char *hash = "12345";
+	char *URL = "www.w3school.com";
 	sqlite3 *db;
 	char *zErrMsg = 0;
 	int rc;
 	char *sql;
-	const char *string = "The quick brown fox jumped over the lazy dog's back";
+	const char* data;
 
 	// open sqlite3 database "URL_Records"
 	rc = sqlite3_open("URL_Records.db", &db);
@@ -57,26 +66,25 @@ int main(int argc, char* argv[]) {
 	if (rc) {
 		fprintf(stderr, "can't open");
 		exit(0);
-	} else {
-		fprintf(stdout, "opened");
 	}
 
 	//query to create "Records" table if not exists.
 	sql = "CREATE TABLE if not exists Records("
-			"Id INTEGER PRIMARY KEY  AUTOINCREMENT,"
-			"URL           TEXT    NOT NULL,"
-			"Hash          CHAR(5)     NOT NULL);";
-	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+			"Hash CHAR(5) PRIMARY KEY    NOT NULL,"
+			"URL           TEXT    NOT NULL);";
+
+	rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
 
 	//Execute the sql statement.
 	if (rc != SQLITE_OK) {
 		fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
-	} else {
-		fprintf(stdout, "Table created successfully\n");
 	}
 
-	sql = "SELECT * from Records";
+	char sql_1[255] = "SELECT URL from Records where Hash = ";
+	strcat(sql_1, hash);
+
+	sql = sql_1;
 
 	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
 	//Execute the sql statement.
