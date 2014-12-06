51a402137335d91412274696c4251257e5af5f94
tharindu99 <madushanka.best@gmail.com>
2014-12-06 23:58:30 +0530

    change the hash generater print option

--- TinyURL/src/TinyURL.c
+++ TinyURL/src/TinyURL.c
@@ -1,57 +1,49 @@
 /*
  ============================================================================
- Name        : TinyURL.c
- Author      : Tharindu  Madushanka
- Version     :
- Copyright   : Your copyright notice
+ Name : TinyURL.c
+ Author : Tharindu Madushanka
+ Version :
+ Copyright : Your copyright notice
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sqlite3.h>
 #include <string.h>
 #include <openssl/sha.h>
-
 static int callback(void *data, int argc, char **argv, char **azColName) {
-	// this function for print the database retrivals.
+// this function for print the database retrivals.
 	int i;
-	printf("%s\n :",argv[0]);
+	if(argv[0]==NULL)printf("empty");
+	printf("%s\n :", argv[0]);
 	/*
 	 * if you want to print all the retrivals from query
-	for (i = 0; i < argc; i++) {
-		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-	}*/
+	 for (i = 0; i < argc; i++) {
+	 printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	 }*/
 	return 0;
 }
-
 static char* hash_generator(char *word) {
-	//this function for create the hash value for string
+//this function for create the hash value for string
 	int i = 0;
 	unsigned char temp[SHA_DIGEST_LENGTH];
 	char buf[SHA_DIGEST_LENGTH * 2];
-
 	memset(buf, 0x0, SHA_DIGEST_LENGTH * 2);
 	memset(temp, 0x0, SHA_DIGEST_LENGTH);
-
-	//hash generate using SHA1 algorithm
+//hash generate using SHA1 algorithm
 	SHA1((unsigned char *) word, strlen(word), temp);
-
 	for (i = 0; i < 4; i++) {
 		sprintf((char*) &(buf[i]), "%02x", temp[i]);
 	}
+	printf("hash for %s is : %s\n",word,buf);
 	return buf;
-
 }
 /*
-static void show(char *hash_value){
-
-}*/
-
+ static void show(char *hash_value){
+ }*/
 int main(int argc, char* argv[]) {
-
-	char *hash = "12345";
+	char *hash = "12346";
 	char *URL = "www.w3school.com";
 	sqlite3 *db;
 	char *zErrMsg = 0;
@@ -59,35 +51,30 @@ int main(int argc, char* argv[]) {
 	char *sql;
 	const char* data;
 
-	// open sqlite3 database "URL_Records"
-	rc = sqlite3_open("URL_Records.db", &db);
+	hash_generator(URL);
 
-	// if db exist it will open otherwise create a db
+// open sqlite3 database "URL_Records"
+	rc = sqlite3_open("URL_Records.db", &db);
+// if db exist it will open otherwise create a db
 	if (rc) {
 		fprintf(stderr, "can't open");
 		exit(0);
 	}
-
-	//query to create "Records" table if not exists.
+//query to create "Records" table if not exists.
 	sql = "CREATE TABLE if not exists Records("
-			"Hash CHAR(5) PRIMARY KEY    NOT NULL,"
-			"URL           TEXT    NOT NULL);";
-
-	rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
-
-	//Execute the sql statement.
+			"Hash CHAR(5) PRIMARY KEY NOT NULL,"
+			"URL TEXT NOT NULL);";
+	rc = sqlite3_exec(db, sql, callback, (void*) data, &zErrMsg);
+//Execute the sql statement.
 	if (rc != SQLITE_OK) {
 		fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
 	}
-
 	char sql_1[255] = "SELECT URL from Records where Hash = ";
 	strcat(sql_1, hash);
-
 	sql = sql_1;
-
 	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-	//Execute the sql statement.
+//Execute the sql statement.
 	if (rc != SQLITE_OK) {
 		fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
@@ -95,9 +82,5 @@ int main(int argc, char* argv[]) {
 		fprintf(stdout, "operation successfully\n");
 	}
 	sqlite3_close(db);
-
-	// hash
-
-	printf("SHA1 is %s\n", hash_generator("tharirrndu"));
 
 }
