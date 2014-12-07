2d345fe60e7915aeb2d854183b2933c3423d3678
tharindu99 <madushanka.best@gmail.com>
2014-12-08 01:01:04 +0530

    finalise buisness logic

--- TinyURL/src/TinyURL.c
+++ TinyURL/src/TinyURL.c
@@ -13,15 +13,14 @@
 #include <string.h>
 #include <openssl/sha.h>
 
-
-void quit(){
+void quit() {
+	printf("budu saranayi !!");
 	exit(1);
 }
 
 static int callback(void *data, int argc, char **argv, char **azColName) {
 // this function for print the database retrivals.
-	printf("hiiiiii");
-	printf("%s\n :", argv[0]);
+	printf("%s :\n", argv[0]);
 	/*
 	 * if you want to print all the retrivals from query
 	 * int i;
@@ -62,50 +61,51 @@ int main(int argc, char* argv[]) {
 	const char* data;
 
 	// open sqlite3 database "URL_Records"
-		rc = sqlite3_open("URL_Records.db", &db);
+	rc = sqlite3_open("URL_Records.db", &db);
 	// if db exist it will open otherwise create a db
-		if (rc) {
-			fprintf(stderr, "can't open");
-			exit(0);
-		}
+	if (rc) {
+		fprintf(stderr, "can't open");
+		exit(0);
+	}
 	//query to create "Records" table if not exists.
-		sql = "CREATE TABLE if not exists Records("
-				"Hash CHAR(5) PRIMARY KEY NOT NULL,"
-				"URL TEXT NOT NULL);";
-		rc = sqlite3_exec(db, sql, callback, (void*) data, &zErrMsg);
+	sql = "CREATE TABLE if not exists Records("
+			"Hash CHAR(5) PRIMARY KEY NOT NULL,"
+			"URL TEXT NOT NULL);";
+	rc = sqlite3_exec(db, sql, callback, (void*) data, &zErrMsg);
 	//Execute the sql statement.
-		if (rc != SQLITE_OK) {
-			fprintf(stderr, "SQL error1: %s\n", zErrMsg);
-			sqlite3_free(zErrMsg);
-		}
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "SQL error1: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
+	}
 
-while(1){
-	int a;
-	printf("Enter 1 to add URl , 2 to hash and other to quit :");
+	while (1) {
+		int a;
+		printf("Enter 1 to add URl , 2 to hash and other to quit :");
 
-	printf("got the number: %d\n", scanf("%d", &a));
+		scanf("%d", &a);
 
-	if (a == 1) {
-		// insert URL
-		printf("Enter the URL :");
-		scanf("%s", &URL_in);
-		URL = URL_in;
-		hash = hash_generator(URL);
-		//strcpy(hash, hash_generator(URL));
+		if (a == 1) {
+			// insert URL
+			printf("Enter the URL :");
+			scanf("%s", &URL_in);
+			URL = URL_in;
+			hash = hash_generator(URL);
+			//strcpy(hash, hash_generator(URL));
 
-		printf("the tiny url for that : www.myURl/%s\n", hash);
+			printf("the tiny url for that : www.myURl/%s\n", hash);
 
-		char sql_in_1[255] = "INSERT OR IGNORE INTO Records(Hash,URL) VALUES ('";
-		strcat(sql_in_1, hash);
-		strcat(sql_in_1, "'");
-		strcat(sql_in_1, ",'");
-		strcat(sql_in_1, URL);
-		strcat(sql_in_1, "');");
+			char sql_in_1[255] =
+					"INSERT OR IGNORE INTO Records(Hash,URL) VALUES ('";
+			strcat(sql_in_1, hash);
+			strcat(sql_in_1, "'");
+			strcat(sql_in_1, ",'");
+			strcat(sql_in_1, URL);
+			strcat(sql_in_1, "');");
 
-		//printf("done %s\n ", sql_in_1);
+			//printf("done %s\n ", sql_in_1);
 
-		rc = sqlite3_exec(db, sql_in_1, callback, 0, &zErrMsg);
-		//Execute the sql statement.
+			rc = sqlite3_exec(db, sql_in_1, callback, 0, &zErrMsg);
+			//Execute the sql statement.
 			if (rc != SQLITE_OK) {
 				fprintf(stderr, "SQL error2: %s\n", zErrMsg);
 				sqlite3_free(zErrMsg);
@@ -113,27 +113,26 @@ while(1){
 				fprintf(stdout, "database updated\n");
 			}
 
-	} else if (a == 2) {
-		printf("Enter the Hash :");
-		scanf("%s", &hash_in);
-		hash = hash_in;
+		} else if (a == 2) {
+			printf("Enter the Hash :");
+			scanf("%s", &hash_in);
+			hash = hash_in;
 
-		char sql_1[255] = "SELECT URL from Records where Hash = '";
+			char sql_1[255] = "SELECT URL from Records where Hash = '";
 			strcat(sql_1, hash);
 			strcat(sql_1, "';");
 			sql = sql_1;
 
+			printf("The Url is : \n");
 			rc = sqlite3_exec(db, sql_1, callback, 0, &zErrMsg);
-					//Execute the sql statement.
-						if (rc != SQLITE_OK) {
-							fprintf(stderr, "SQL error2: %s\n", zErrMsg);
-							sqlite3_free(zErrMsg);
-						} else {
-							fprintf(stdout, "URL is : ");
-						}
-	}else {
-		break;
-	}
+			//Execute the sql statement.
+			if (rc != SQLITE_OK) {
+				fprintf(stderr, "SQL error2: %s\n", zErrMsg);
+				sqlite3_free(zErrMsg);
+			}
+		} else {
+			break;
+		}
 
 	}
 	sqlite3_close(db);
