99f0c890415c70adcea1de678bb4b2315b687ab6
tharindu99 <madushanka.best@gmail.com>
2014-12-08 00:45:25 +0530

    user inout handling and finalising 1st level buisness logic

--- TinyURL/src/TinyURL.c
+++ TinyURL/src/TinyURL.c
@@ -12,19 +12,25 @@
 #include <sqlite3.h>
 #include <string.h>
 #include <openssl/sha.h>
+
+
+void quit(){
+	exit(1);
+}
+
 static int callback(void *data, int argc, char **argv, char **azColName) {
 // this function for print the database retrivals.
-	int i;
-	if(argv[0]==NULL)printf("empty");
+	printf("hiiiiii");
 	printf("%s\n :", argv[0]);
 	/*
 	 * if you want to print all the retrivals from query
+	 * int i;
 	 for (i = 0; i < argc; i++) {
 	 printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
 	 }*/
 	return 0;
 }
-static char* hash_generator(char *word) {
+char* hash_generator(char *word) {
 //this function for create the hash value for string
 	int i = 0;
 	unsigned char temp[SHA_DIGEST_LENGTH];
@@ -36,60 +42,100 @@ static char* hash_generator(char *word) {
 	for (i = 0; i < 4; i++) {
 		sprintf((char*) &(buf[i]), "%02x", temp[i]);
 	}
-	printf("hash for %s is : %s\n",word,buf);
-	return buf;
+
+	char* out_hash = malloc(strlen(buf) + 1);
+	strcpy(out_hash, buf);
+
+	return out_hash;
 }
 /*
  static void show(char *hash_value){
  }*/
 int main(int argc, char* argv[]) {
-	char *hash = "12344";
-	char *URL = "www.w3school.com";
+
+	char hash_in[255], URL_in[255];
+	char *hash, *URL;
 	sqlite3 *db;
 	char *zErrMsg = 0;
 	int rc;
 	char *sql;
 	const char* data;
 
-	hash_generator(URL);
+	// open sqlite3 database "URL_Records"
+		rc = sqlite3_open("URL_Records.db", &db);
+	// if db exist it will open otherwise create a db
+		if (rc) {
+			fprintf(stderr, "can't open");
+			exit(0);
+		}
+	//query to create "Records" table if not exists.
+		sql = "CREATE TABLE if not exists Records("
+				"Hash CHAR(5) PRIMARY KEY NOT NULL,"
+				"URL TEXT NOT NULL);";
+		rc = sqlite3_exec(db, sql, callback, (void*) data, &zErrMsg);
+	//Execute the sql statement.
+		if (rc != SQLITE_OK) {
+			fprintf(stderr, "SQL error1: %s\n", zErrMsg);
+			sqlite3_free(zErrMsg);
+		}
 
-// open sqlite3 database "URL_Records"
-	rc = sqlite3_open("URL_Records.db", &db);
-// if db exist it will open otherwise create a db
-	if (rc) {
-		fprintf(stderr, "can't open");
-		exit(0);
-	}
-//query to create "Records" table if not exists.
-	sql = "CREATE TABLE if not exists Records("
-			"Hash CHAR(5) PRIMARY KEY NOT NULL,"
-			"URL TEXT NOT NULL);";
-	rc = sqlite3_exec(db, sql, callback, (void*) data, &zErrMsg);
-//Execute the sql statement.
-	if (rc != SQLITE_OK) {
-		fprintf(stderr, "SQL error: %s\n", zErrMsg);
-		sqlite3_free(zErrMsg);
-	}
-	char sql_1[255] = "SELECT URL from Records where Hash = ";
-	strcat(sql_1, hash);
-	sql = sql_1;
-	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-//Execute the sql statement.
-	if (rc != SQLITE_OK) {
-		fprintf(stderr, "SQL error: %s\n", zErrMsg);
-		sqlite3_free(zErrMsg);
-	} else {
-		fprintf(stdout, "operation successfully\n");
+while(1){
+	int a;
+	printf("Enter 1 to add URl , 2 to hash and other to quit :");
+
+	printf("got the number: %d\n", scanf("%d", &a));
+
+	if (a == 1) {
+		// insert URL
+		printf("Enter the URL :");
+		scanf("%s", &URL_in);
+		URL = URL_in;
+		hash = hash_generator(URL);
+		//strcpy(hash, hash_generator(URL));
+
+		printf("the tiny url for that : www.myURl/%s\n", hash);
+
+		char sql_in_1[255] = "INSERT OR IGNORE INTO Records(Hash,URL) VALUES ('";
+		strcat(sql_in_1, hash);
+		strcat(sql_in_1, "'");
+		strcat(sql_in_1, ",'");
+		strcat(sql_in_1, URL);
+		strcat(sql_in_1, "');");
+
+		//printf("done %s\n ", sql_in_1);
+
+		rc = sqlite3_exec(db, sql_in_1, callback, 0, &zErrMsg);
+		//Execute the sql statement.
+			if (rc != SQLITE_OK) {
+				fprintf(stderr, "SQL error2: %s\n", zErrMsg);
+				sqlite3_free(zErrMsg);
+			} else {
+				fprintf(stdout, "database updated\n");
+			}
+
+	} else if (a == 2) {
+		printf("Enter the Hash :");
+		scanf("%s", &hash_in);
+		hash = hash_in;
+
+		char sql_1[255] = "SELECT URL from Records where Hash = '";
+			strcat(sql_1, hash);
+			strcat(sql_1, "';");
+			sql = sql_1;
+
+			rc = sqlite3_exec(db, sql_1, callback, 0, &zErrMsg);
+					//Execute the sql statement.
+						if (rc != SQLITE_OK) {
+							fprintf(stderr, "SQL error2: %s\n", zErrMsg);
+							sqlite3_free(zErrMsg);
+						} else {
+							fprintf(stdout, "URL is : ");
+						}
+	}else {
+		break;
 	}
 
-	char sql_in_1[255] = "INSERT OR IGNORE INTO Records(Hash,URL)VALUES(";
-	strcat(sql_in_1,hash_generator(URL));
-	strcat(sql_in_1,",'");
-	strcat(sql_in_1,URL);
-	strcat(sql_in_1,"');");
-
-	printf("done %s\n ",sql_in_1);
-	rc = sqlite3_exec(db, sql_in_1, callback, 0, &zErrMsg);
+	}
 	sqlite3_close(db);
-
+	quit();
 }
